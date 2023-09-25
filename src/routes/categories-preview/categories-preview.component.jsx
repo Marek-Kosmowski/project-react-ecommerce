672746324249a14b17dcf451e9747d0843ba2537
Marek-Kosmowski <kosmowski.marek@gmail.com>
2023-09-25 20:30:03 +0200

    Add spinner for loading categories-preview and category component

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,20 +1,30 @@
 import { Fragment } from 'react';
 import { useSelector } from 'react-redux/es/hooks/useSelector';
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import {
+  selectCategoriesMap,
+  selectCategoriesIsLoading,
+} from '../../store/categories/category.selector';
 
 import CategoryPreview from '../../components/category-preview/category-preview.component';
 
+import Spinner from '../../components/spinner/spinner.component';
+
 const CategoriesPreview = () => {
   const getCategoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
   return (
     // <> => shortand for fragment
     <Fragment>
-      {Object.keys(getCategoriesMap).map((title) => {
-        const products = getCategoriesMap[title];
-        return (
-          <CategoryPreview key={title} title={title} products={products} />
-        );
-      })}
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        Object.keys(getCategoriesMap).map((title) => {
+          const products = getCategoriesMap[title];
+          return (
+            <CategoryPreview key={title} title={title} products={products} />
+          );
+        })
+      )}
     </Fragment>
   );
 };
--- src/store/store.js
+++ src/store/store.js
@@ -23,7 +23,7 @@ const middleWares = [process.env.NODE_ENV !== 'production' && logger, thunk].fil
 const persistConfig = {
     key: 'root',
     storage,
-    blacklist: ['user'],
+    whitelist: ['cart'],
 }
 
 
